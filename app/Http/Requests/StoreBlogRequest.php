2bd88385f227121b34701c80e3564841e0e0ecdc
jiumu <jiumu@nukaka.com>
2022-07-15 16:36:04 +0800

    use class

--- app/Http/Requests/StoreBlogRequest.php
+++ app/Http/Requests/StoreBlogRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class StoreBlogRequest extends FormRequest
 {
